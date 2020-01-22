a62116baf790238b60fc48ee49171fd5033f6705
steven.wang <steven.wang@bigcommerce.com>
2020-01-22 14:09:43 +1100

    infra_2186 initial commit

---
+++ process_yaml.py
@@ -0,0 +1,37 @@
+import yaml
+
+switcher = {
+        's': 1,
+        'm': 60,
+        'h': 3600,
+        'd': 86400,
+        'y': 31536000,
+    }
+
+def convert_retention_to_int(retention):
+    if(len(retention)== 0):
+        raise Exception("Empty string")
+    if retention.isdigit():
+        return int(retention)
+    unit = retention[-1]
+    exception_msg = "Invalid date"
+    multiplier = switcher.get(unit, exception_msg)
+    if(multiplier == exception_msg):
+        raise Exception(exception_msg)
+    value = int(retention[0:-1])
+    return value * multiplier
+
+with open(r'type_graphite_storage.yaml') as file:
+    content = yaml.full_load(file)
+    schemas = content['bigcommerce_graphite_gcp::roles::storage::graphite_schemas']
+    for schema, info in schemas.items():
+        retentions = info['retentions']
+        path = "output/" + schema
+        f = open(path, "w+")
+        for retention in retentions:
+            freq_str, duration_str = retention.split(':')
+            freq = convert_retention_to_int(freq_str)
+            duration = convert_retention_to_int(duration_str)
+            output_string = "retention: {} secondsPerPoint: {}\n".format(duration, freq)
+            f.write(output_string)
+        f.close()

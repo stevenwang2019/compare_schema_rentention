3cd30039320d08dbb1baa173cff9223d598514d1
steven.wang <steven.wang@bigcommerce.com>
2020-01-22 15:15:13 +1100

    refactor code

--- process_yaml.py
+++ process_yaml.py
@@ -22,20 +22,34 @@ def convert_retention_to_int(retention):
     value = int(retention[0:-1])
     return value * multiplier
 
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def convert_retention_to_string(retention):
+    freq_str, duration_str = retention.split(':')
+    freq = convert_retention_to_int(freq_str)
+    duration = convert_retention_to_int(duration_str)
+    # output_string = "retention: {} secondsPerPoint: {}\n".format(duration, freq)
+    return "{} {}".format(duration, freq)
+
+def write_to_file(path, pattern ,list):
+    f = open(path, "w+")
+    for value in list:
+        f.write(value+"\n")
+    f.close()
+
 with open(r'type_graphite_storage.yaml') as file:
     content = yaml.full_load(file)
     schemas = content['bigcommerce_graphite_gcp::roles::storage::graphite_schemas']
     directory = 'output/'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    create_dir(directory)
     for schema, info in schemas.items():
         retentions = info['retentions']
-        path = directory + schema
-        f = open(path, "w+")
+        list = []
         for retention in retentions:
-            freq_str, duration_str = retention.split(':')
-            freq = convert_retention_to_int(freq_str)
-            duration = convert_retention_to_int(duration_str)
-            output_string = "retention: {} secondsPerPoint: {}\n".format(duration, freq)
-            f.write(output_string)
-        f.close()
+            output_string = convert_retention_to_string(retention)
+            list.append(output_string)
+        #compare_whisper_info(schema, list)
+        write_to_file(directory + schema, info['pattern'], list)
+
